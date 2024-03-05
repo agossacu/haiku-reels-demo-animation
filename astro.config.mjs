788dbcd9a82f9e2ec818c282f1ef942b50e6f3f9
Joaquin Blanco <blancojoaquin77@gmail.com>
2024-03-04 21:52:32 -0300

    se ocultaron algunos backgrounds

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://joackob.github.io",
-	base: "/haiku-reels-astro",
+	base: "/haiku-reels-demo",
 	integrations: [mdx(), sitemap(), tailwind()],
 });
